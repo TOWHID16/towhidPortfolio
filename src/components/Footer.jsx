8ea945514bcec68528f6c8e6affc3059f85826b4
Towhidur Rahman <towhidasif@gmail.com>
2025-08-25 20:12:41 +0600

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import LinkedinIcon from './icons/LinkedinIcon';
 import TwitterIcon from './icons/TwitterIcon';
 import InstagramIcon from './icons/InstagramIcon';
 import EmailIcon from './icons/EmailIcon';
-//import YoutubeIcon from './icons/YoutubeIcon';
 import GitHubIcon from './icons/GitHubIcon'; 
 
 const Footer = () => {
@@ -12,27 +11,41 @@ const Footer = () => {
     { href: 'https://x.com/towhidasif29?s=21', icon: <TwitterIcon width={20} /> },
     { href: 'https://www.instagram.com/towhidasif29?igsh=MWVleWZwZDc1cHlpbg%3D%3D&utm_source=qr', icon: <InstagramIcon width={20} /> },
     { href: 'mailto:towhidasif@gmail.com', icon: <EmailIcon width={20} /> },
-    //{ href: 'https://www.youtube.com/@procodrr', icon: <YoutubeIcon width={20} /> },
     { href: 'https://github.com/towhid16', icon: <GitHubIcon width={20} /> },
   ];
+
+  // COMMENT: This function will handle the smooth scroll to the top of the page.
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // This makes the scroll smooth
+    });
+  };
+
   return (
     <footer className="flex flex-col items-center pb-10">
-      {/* <p className="text-xs text-gray-300">Towhid Asif © 2025</p> */}
-      
-      {/* COMMENT: This is the new section for your profile picture. */}
-      <div className="my-4">
+      {/* COMMENT: The image is now wrapped in a div to handle the click and hover effects. */}
+      <div 
+        className="my-4 relative group cursor-pointer"
+        onClick={handleScrollToTop} // COMMENT: The scroll function is called when this is clicked.
+      >
         <img
-          // COMMENT: IMPORTANT! Change this src to the actual path of your photo.
-          // For example, if your photo is in `public/assets/images/`, the path would be `/assets/images/your-photo.jpg`
           src="/assets/images/footer1.png"
           alt="Towhid Asif"
           className="w-12 h-12 rounded-full border-2 border-gray-500 object-cover"
         />
+        {/* COMMENT: This is the tooltip that appears on hover. */}
+        <div className="absolute top-1/2 -translate-y-1/2 left-full ml-4 hidden group-hover:flex items-center">
+          <div className="w-3 h-3 -mr-2 rotate-45 bg-[#1788ae]"></div>
+          <span className="relative z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-[#1788ae] shadow-lg rounded-md">
+            To Top
+          </span>
+        </div>
       </div>
 
-      <p className="text-xs text-gray-300">Towhid Asif © 2025</p> <br />
+      <p className="text-xs text-gray-300">Towhid Asif © 2025</p>
 
-      <ul className="mx-auto flex items-center justify-center gap-6 text-[#b0b2c3]">
+      <ul className="mx-auto flex items-center justify-center gap-6 text-[#b0b2c3] mt-4">
           {socialLinks.map((link, index) => (
             <li key={index}>
               <a href={link.href} target="_blank" rel="noopener noreferrer" className="hover:text-white">
